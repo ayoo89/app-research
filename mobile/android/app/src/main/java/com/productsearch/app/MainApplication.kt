2c89f1768a75d8c6792d050ae22037add13b0b46
Product Search <deploy@productsearch.app>
2026-04-11 07:09:07 +0100

    fix: pin reanimated to 3.6.3 (RN 0.73 compatible), revert JSI subproject approach

--- mobile/android/app/src/main/java/com/productsearch/app/MainApplication.kt
+++ mobile/android/app/src/main/java/com/productsearch/app/MainApplication.kt
@@ -5,11 +5,9 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeHost
 import com.facebook.react.ReactPackage
-import com.facebook.react.bridge.JSIModulePackage
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
 import com.facebook.react.defaults.DefaultReactNativeHost
 import com.facebook.soloader.SoLoader
-import com.swmansion.reanimated.ReanimatedJSIModulePackage
 
 class MainApplication : Application(), ReactApplication {
 
@@ -23,10 +21,6 @@ class MainApplication : Application(), ReactApplication {
 
             override val isNewArchEnabled: Boolean = false
             override val isHermesEnabled: Boolean = true
-
-            // Required for react-native-reanimated worklets (JSI bridge)
-            override fun getJSIModulePackage(): JSIModulePackage =
-                ReanimatedJSIModulePackage()
         }
 
     override val reactHost: ReactHost
